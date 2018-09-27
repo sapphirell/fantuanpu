2c675f3a81d4322985245e6f0facc7de06449ffb
sapphirell <hentai@fantuanpu.com>
2018-09-27 15:57:04 +0800

    头像大小

--- resources/views/PC/Forum/TopPosts.blade.php
+++ resources/views/PC/Forum/TopPosts.blade.php
@@ -1,13 +1,13 @@
 <div class="node_father">
     @foreach($data['new_reply'] as $item)
-        <div class="shadow" style="
+        <div class="shadow-purple" style="
         width: 712px;
         background: #ffffff;
         margin-bottom: 10px;
         border-radius: 5px;
         padding: 10px;
 ">
-            {{avatar($item->authorid,'small',100)}}
+            {{avatar($item->authorid,'35',100,'avatar-tab','small')}}
             <a href="/thread-{{$item->tid}}-1.html" style="display: inline-block">{{$item->subject}}</a>
             <p style="display: inline-block">{{date("Y-m-d",$item->dateline)}}</p>
         </div>
