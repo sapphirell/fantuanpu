e94190f8fd919d48934af13f74f623f39f0f7d9a
sapphirell <hentai@fantuanpu.com>
2019-02-06 15:49:06 +0800

    缓存

--- resources/views/PC/Common/HtmlHead.blade.php
+++ resources/views/PC/Common/HtmlHead.blade.php
@@ -30,7 +30,7 @@
 <script src="/Static/Script/layer/layer.js"></script>
 {{--<script src="https://apps.bdimg.com/libs/layer/2.1/layer.js"></script>--}}
 
-<script src="/Static/Script/common.js?date=20181108.2"></script>
+<script src="/Static/Script/common.js?date=20190108.2"></script>
 
 <link rel="stylesheet" type="text/css" href="/Static/Style/mc.css?date=20190101.1">
 <link rel="stylesheet" type="text/css" href="/Static/Style/Web/style.css?date=20181108.2">
