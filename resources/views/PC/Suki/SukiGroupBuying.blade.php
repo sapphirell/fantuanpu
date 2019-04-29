aa02052148223ff0c9b437065a7b150ab1573b57
sapphirell <hentai@fantuanpu.com>
2019-04-29 20:12:03 +0800

    css

--- resources/views/PC/Suki/SukiGroupBuying.blade.php
+++ resources/views/PC/Suki/SukiGroupBuying.blade.php
@@ -9,21 +9,24 @@
     </nav>
     <div style="display: flex;justify-content: flex-start;">
         @foreach($data["items"] as $item)
-            <div style="display: inline-block;float: left;padding: 10px;background: #FFFFFF;margin: 10px;box-shadow: 0 0 10px #ececec;border-radius: 5px;">
-                <p>名称:{{$item["item_name"]}}</p>
-                <p>最低成团数: {{$item["min_members"]}}个</p>
-                <p>当前:{{$item["follow"]}}人购买了 {{$item["item_count"]}} 个</p>
-                <p>运费:{{$item["item_freight"]}}</p>
-                <p>辛苦费/次:{{$item["premium"]}}</p>
+            <div style="display: inline-block;width:175px;float: left;padding: 10px;background: #FFFFFF;margin: 10px;box-shadow: 0 0 10px #ececec;border-radius: 5px;">
+                <p class="aline">{{$item["item_name"]}}</p>
+                <p class="aline">最低成团数: {{$item["min_members"]}}个</p>
+                <p class="aline">当前:{{$item["follow"]}}人购买了 {{$item["item_count"]}} 个</p>
+                <p class="aline">运费:{{$item["item_freight"]}}</p>
+                <p class="aline">辛苦费/次:{{$item["premium"]}}</p>
                 <img src="{{$item["item_image"][0]}}" style="width: 155px;height: 107px">
                 <p>可选尺码</p>
                 @foreach($item["item_size"] as $value)
                     <span>{{$value}}</span>
                 @endforeach
                 <p>可选颜色</p>
-                @foreach($item["item_color"] as $value)
-                    <span>{{$value}}</span>
-                @endforeach
+                <div  class="aline">
+                    @foreach($item["item_color"] as $value)
+                        <span>{{$value}}</span>
+                    @endforeach
+                </div>
+
                 <br>
                 <a href="/suki_group_buying_item_info?item_id={{$item["id"]}}" style="    margin-top: 10px;background: #f9c8c9;width: 100%;display: block;color: #fff;text-align: center;background-color: #ffb6c7;background-image: linear-gradient(90deg, #fbded9 0%, #ffa5b2 93%);">查看详情</a>
             </div>
