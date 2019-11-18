62ee98fc2cb43c2a4ba03f41018ec3c3456b3637
sapphirell <hentai@fantuanpu.com>
2019-11-18 21:24:14 +0800

    task界面

--- resources/views/PC/Suki/SukiTaskList.blade.php
+++ resources/views/PC/Suki/SukiTaskList.blade.php
@@ -23,43 +23,69 @@
             <button
                     style="    border: 0px;color: #70748c;background-color: #e9ecef;padding: 3px 15px;margin-left: 0px;"
                     id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                @if($data['type']=='active')
+                @if($data['request']['type'] =='active')
                     正在进行
                 @else
                     接取任务
                 @endif
             </button>
             <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                <a href="/suki_show_task_list" class="dropdown-item" href="#">本期团购</a>
-                <a href="/suki_show_task_list" class="dropdown-item" href="#">现货</a>
+                <a href="/suki_show_task_list" class="dropdown-item" href="#">接取任务</a>
+                <a href="/suki_show_task_list?type=active" class="dropdown-item" href="#">我的任务</a>
             </div>
         </div>
 
     </nav>
     <div style="margin-left: 8px;margin-right: 8px">
-        @foreach($data["task_list"] as $task)
-            <div style="border-bottom: 1px dashed #e4e4e4;padding-bottom: 5px;padding-top: 5px">
-                <p>
-                    <span style="font-weight: 800;">{{$task["task_name"]}}</span>
-                </p>
-                <p style="padding-left: 5px;">
-                    <span class="content_box_title">任务描述</span>
-                    <span class="content_box_body">{{$task["task_description"]}}</span>
-                </p>
-                <p style="padding-left: 5px;padding-bottom: 5px;">
-                    <span class="content_box_body" style="color: #F28A96;font-size: 14px;">
-                        @foreach($task["task_gift"] as $key => $num)
-                            {{$key}}+{{$num}}
-                        @endforeach
-                    </span>
-                </p>
-                @if($task->is_receive)
-                    <span>已经领取过</span>
-                @else
-                <a class="take_task" task_id="{{$task->id}}" style="">领取</a>
-                @endif
-            </div>
-        @endforeach
+        <div>
+            @if($data["request"]["type"] == "active")
+                @foreach($data["my_task"] as $value)
+                    <div>
+                        <p>
+                            <span style="font-weight: 800;">{{$value->task_name}} []</span>
+                        </p>
+                        <p style="padding-left: 5px;">
+                            <span class="content_box_title">任务描述</span>
+                            <span class="content_box_body">{{$value->task_description}}</span>
+                        </p>
+                        <p style="padding-left: 5px;">
+                            <span class="content_box_title">奖励</span>
+                            <span class="content_box_body"  style="color: #F28A96;font-size: 14px;">
+
+                                @foreach($value->task_gift as $key => $gift)
+                                    {{$gift}}{{\App\Http\DbModel\CommonMemberCount::$extcredits[$key]}}
+                                @endforeach
+                            </span>
+                        </p>
+                    </div>
+                @endforeach
+            @else
+                @foreach($data["task_list"] as $task)
+                    <div style="border-bottom: 1px dashed #e4e4e4;padding-bottom: 5px;padding-top: 5px">
+                        <p>
+                            <span style="font-weight: 800;">{{$task["task_name"]}}</span>
+                        </p>
+                        <p style="padding-left: 5px;">
+                            <span class="content_box_title">任务描述</span>
+                            <span class="content_box_body">{{$task["task_description"]}}</span>
+                        </p>
+                        <p style="padding-left: 5px;padding-bottom: 5px;">
+                        <span class="content_box_body" style="color: #F28A96;font-size: 14px;">
+                            @foreach($task["task_gift"] as $key => $num)
+                                {{$key}}+{{$num}}
+                            @endforeach
+                        </span>
+                        </p>
+                        @if($task->is_receive)
+                            <span>已经领取过</span>
+                        @else
+                            <a class="take_task" task_id="{{$task->id}}" style="">领取</a>
+                        @endif
+                    </div>
+                @endforeach
+            @endif
+        </div>
+
     </div>
 
 </div>
